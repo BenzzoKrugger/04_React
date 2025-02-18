a5499a18854c80c05868022931ec9a8ddcdb52d6
BenzzoKrugger <benzzo.krugger@proton.me>
2025-02-18 22:32:45 +0100

    implement responsive navbar with toggle functionality

--- src/assets/Navbar.jsx
+++ src/assets/Navbar.jsx
@@ -1,27 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleNav = () => setIsOpen(!isOpen);
+
   return (
     <div className="mx-auto flex h-24 max-w-[1240px] items-center justify-between px-4 text-white">
       <h1 className="text-3xl font-bold text-[#00df9a]">REACT.</h1>
-      <ul className="flex hidden">
+      <ul className="hidden md:flex">
         <li className="p-4">Home</li>
         <li className="p-4">Company</li>
         <li className="p-4">Resources</li>
         <li className="p-4">About</li>
         <li className="p-4">Contact</li>
       </ul>
-      <div>
-        <AiOutlineMenu size={20} />
+      <div onClick={toggleNav} className="block cursor-pointer md:hidden">
+        {isOpen ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20}/>}
       </div>
-      <div className="fixed top-0 left-0 h-full w-2/3 border-r border-r-gray-900 bg-[#000300]">
-        <h1 className="text-3xl font-bold text-[#00df9a] m-4">REACT.</h1>
-        <ul className="uppercase p-4">
-          <li className="p-4 border-b border-gray-700">Home</li>
-          <li className="p-4 border-b border-gray-700">Company</li>
-          <li className="p-4 border-b border-gray-700">Resources</li>
-          <li className="p-4 border-b border-gray-700">About</li>
+      <div className={isOpen ? 'fixed top-0 left-0 h-full w-2/3 border-r border-r-gray-900 bg-[#000300] ease-in-out duration-600' : 'fixed left-[-100%]'}>
+        <h1 className="m-4 text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <ul className="p-4 uppercase">
+          <li className="border-b border-gray-700 p-4">Home</li>
+          <li className="border-b border-gray-700 p-4">Company</li>
+          <li className="border-b border-gray-700 p-4">Resources</li>
+          <li className="border-b border-gray-700 p-4">About</li>
           <li className="p-4">Contact</li>
         </ul>
       </div>
